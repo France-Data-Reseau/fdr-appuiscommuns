9b35ab4992cbde08ec6ca5006bdc0f5e18619159
Marc Dutoo <marc.dutoo@gmail.com>
2022-04-25 19:11:34 +0200

    moved some files

--- models/apcom/appuiscommuns_supportaerien__expected_stg.sql
+++ models/apcom/appuiscommuns_supportaerien__expected_stg.sql
@@ -1,6 +1,22 @@
 {#
-Parsing de l'attendu _expected du test unitaire de normalization
+Parsing of a priori (made-up), covering examples of the definition / interface.
+Examples have to be **as representative** of all possible data as possible because they are also the basis of the definition.
+For instance, for a commune INSEE id field, they should also include a non-integer value such as 2A035 (Belvédère-Campomoro).
+Methodology :
+1. copy the first line(s) from the specification document
+2. add line(s) to contain further values for until they are covering for all columns
+3. NB. examples specific to each source type are provided in _source_example along their implementation (for which they are covering)
+
+TODO can't be replaced by from_csv because is the actual definition, BUT could be by guided by metamodel !
+{{ apcom_supportaerien_from_csv(ref(model.name[:-4])) }}
+
 #}
+
+{{
+  config(
+    materialized="view"
+  )
+}}
 
 {% set containerUrl = 'http://' + 'datalake.francedatareseau.fr' %}
 {% set typeUrlPrefix = containerUrl + '/dc/type/' %}
@@ -16,11 +32,11 @@ Parsing de l'attendu _expected du test unitaire de normalization
 {% set fieldPrefix = prefix + '__' %}
 {% set idUrlPrefix = typeUrlPrefix + type + '/' %}
 
-{# __expected and NOT __definition else also asks for the fields not provided by this source #}
 select
-    {{ dbt_utils.star(ref('osm_powersupports__supportaerien__expected'),
-        except=[fieldPrefix + 'Id', 'geometry']) }},
-    "{{ fieldPrefix }}Id"::uuid as "{{ fieldPrefix }}Id",
+    {{ dbt_utils.star(ref('appuiscommuns_supportaerien__example'),
+        except=[fieldPrefix + 'Id', 'geometry', fieldPrefix + 'HauteurAppui']) }},
+    "{{ fieldPrefix }}Id"::uuid,
+    "{{ fieldPrefix }}HauteurAppui"::numeric,
     ST_GeomFROMText(geometry, 4326) as geometry -- NOT ::geometry else not the same (srid ?? only visible in binary ::text form : ) therefore except does not work
     -- 0101000000197B8A77DBE0E33F18C25725ECC34740 expected
     -- 0101000020E6100000197B8A77DBE0E33F18C25725ECC34740 actual
@@ -30,4 +46,4 @@ select
     ----appuiscommunssupp__fdrcommune__insee_id as fdrcommune__insee_id
     --appuiscommunssupp__fdrcommune__nom as appuiscommunssupp__commune_nom
     
-    from {{ ref('osm_powersupports__supportaerien__expected') }} -- TODO raw_
+    from {{ ref('appuiscommuns_supportaerien__example') }} -- TODO raw_
