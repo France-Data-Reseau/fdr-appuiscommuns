b55ef9568a96e64fc41549fecbeafc748c2d14e0
Marc Dutoo <marc.dutoo@gmail.com>
2022-04-29 20:13:34 +0200

    dedup : more generic, patched outside within same source

--- macros/apcom/apcom_supportaerien__dedupe_geometry.sql
+++ macros/apcom/apcom_supportaerien__dedupe_geometry.sql
@@ -30,18 +30,20 @@ TODO example
 with link_candidates as (
     -- xs on 1m lines
     select
-        {# % for field in id_and_order_by_fields %}
-          earlier."{{ field }}" as "earlier{{ field }}", later."{{ field }}" as "later{{ field }}" {% if not loop.last%},{% endif %}
-        {% endfor % #}
+        {% for field in id_and_order_by_fields %}
+          earlier."{{ field }}" as "earlier{{ field }}",
+        {% endfor %}
+        {% for field in id_and_order_by_fields %}
+          later."{{ field }}" as "later{{ field }}" {% if not loop.last%},{% endif %}
+        {% endfor %}
+        ---- earlier."{{ src_name_field }}" as "earlier{{ src_name_field }}",
+        ---- earlier."{{ src_id_field }}" as "earlier{{ src_id_field }}", -- to be able to order before group by (TODO Q but does it use it ?)
+        ---- earlier."{{ id_field }}" as "earlier{{ id_field }}",
+        ---- later."{{ src_name_field }}" as "later{{ src_name_field }}",
+        ---- later."{{ src_id_field }}" as "later{{ src_id_field }}",
+        ---- later."{{ id_field }}" as "later{{ id_field }}"
 
-        earlier."{{ src_name_field }}" as "earlier{{ src_name_field }}",
-        earlier."{{ src_id_field }}" as "earlier{{ src_id_field }}", -- to be able to order before group by (TODO Q but does it use it ?)
-        earlier."{{ id_field }}" as "earlier{{ id_field }}",
-        later."{{ src_name_field }}" as "later{{ src_name_field }}",
-        later."{{ src_id_field }}" as "later{{ src_id_field }}",
-        later."{{ id_field }}" as "later{{ id_field }}"
-
-        --, earlier.geometry as earlier_geometry, later.geometry as later_geometry -- for debugging purpose
+        , earlier.geometry as earlier_geometry, later.geometry as later_geometry -- for debugging purpose
     FROM {{ ref(normalized_source_model_name) }} earlier, {{ ref(normalized_source_model_name) }} later
     WHERE earlier."{{ src_name_field }}" < later."{{ src_name_field }}" -- only between different sources
     --and earlier."{{ src_id_field }}" < later."{{ src_id_field }}" -- ONLY IF WERE IN SAME SOURCE assuming _src_id is the model's ordering field
@@ -51,32 +53,64 @@ with link_candidates as (
     --and ST_Expand(earlier.geometry, {{ distance_m }}) && later.geometry -- TODO REQUIRES CONVERT and then ST_Within faster
     -- see https://gis.stackexchange.com/questions/93936/searching-planet-osm-point-by-longitude-and-latitude/93957#93957 https://gis.stackexchange.com/questions/94886/st-expand-return-different-results-depending-on-meters
     --and ST_DWithin(way, ST_Transform(later.geometry, 3857),
-    ORDER BY earlier."{{ src_name_field }}" asc, earlier."{{ src_id_field }}" desc,
-    later."{{ src_name_field }}" asc, later."{{ src_id_field }}" desc
+    ORDER BY
+    {% for order_by_field in order_by_fields %}
+      "earlier{{ order_by_field }}" desc,
+    {% endfor %}
+    {% for order_by_field in order_by_fields %}
+      "later{{ order_by_field }}" desc {% if not loop.last%},{% endif %}
+    {% endfor %}
+    ---- earlier."{{ src_name_field }}" asc, earlier."{{ src_id_field }}" desc,
+    ---- later."{{ src_name_field }}" asc, later."{{ src_id_field }}" desc
 
 ), filtered as (
 
   -- remove lines where later / right part has already been mentioned before as earlier / left part
   -- i.e. previous groups will already have included them :
   select * from link_candidates current_lc
-  where "earlier{{ id_field }}" not in (
+  where "later{{ id_field }}" not in (
     select "later{{ id_field }}" from link_candidates lc_before_current_one
-    WHERE lc_before_current_one."earlier{{ src_name_field }}" < current_lc."later{{ src_name_field }}" -- only between different sources
+    WHERE
+    lc_before_current_one."earlier{{ src_name_field }}" < current_lc."earlier{{ src_name_field }}" -- only between different sources
+    or (lc_before_current_one."earlier{{ src_name_field }}" = current_lc."earlier{{ src_name_field }}"
+      and lc_before_current_one."earlier{{ src_id_field }}" < current_lc."earlier{{ src_id_field }}") -- only between different sources
+    or (lc_before_current_one."earlier{{ src_name_field }}" = current_lc."earlier{{ src_name_field }}"
+      and lc_before_current_one."earlier{{ src_id_field }}" = current_lc."earlier{{ src_id_field }}" -- only between different sources
+      and lc_before_current_one."later{{ src_name_field }}" < current_lc."later{{ src_name_field }}")
+    or (lc_before_current_one."earlier{{ src_name_field }}" = current_lc."earlier{{ src_name_field }}"
+      and lc_before_current_one."earlier{{ src_id_field }}" = current_lc."earlier{{ src_id_field }}" -- only between different sources
+      and lc_before_current_one."later{{ src_name_field }}" = current_lc."later{{ src_name_field }}"
+      and lc_before_current_one."later{{ src_id_field }}" < current_lc."later{{ src_id_field }}")
     --and earlier."{{ src_id_field }}" < later."{{ src_id_field }}" -- ONLY IF WERE IN SAME SOURCE assuming _src_id is the model's ordering field
   )
 
 ), filtered_plus as (
   select * from (
   select
-    "earlier{{ src_name_field }}", "earlier{{ src_id_field }}", "earlier{{ id_field }}",
-    "earlier{{ src_name_field }}" as "later{{ src_name_field }}", "earlier{{ src_id_field }}" as "later{{ src_id_field }}",  "earlier{{ id_field }}" as "later{{ id_field }}"
-    --, earlier_geometry as earlier_geometry, earlier_geometry as later_geometry -- for debugging purpose
+        {% for field in id_and_order_by_fields %}
+          "earlier{{ field }}",
+        {% endfor %}
+        {% for field in id_and_order_by_fields %}
+          "earlier{{ field }}" as "later{{ field }}" {% if not loop.last%},{% endif %}
+        {% endfor %}
+    ---- "earlier{{ src_name_field }}", "earlier{{ src_id_field }}", "earlier{{ id_field }}",
+    ---- "earlier{{ src_name_field }}" as "later{{ src_name_field }}", "earlier{{ src_id_field }}" as "later{{ src_id_field }}",  "earlier{{ id_field }}" as "later{{ id_field }}"
+
+    , earlier_geometry as earlier_geometry, earlier_geometry as later_geometry -- for debugging purpose
     from filtered
   union
   select * from filtered
   ) fp
   -- order of preference of data among lines :
-  order by "earlier{{ src_name_field }}" asc, "earlier{{ src_id_field }}" desc, "later{{ src_name_field }}", "later{{ src_id_field }}"
+  order by
+    {% for order_by_field in order_by_fields %}
+      "earlier{{ order_by_field }}" desc,
+    {% endfor %}
+    {% for order_by_field in order_by_fields %}
+      "later{{ order_by_field }}" desc {% if not loop.last%},{% endif %}
+    {% endfor %}
+  ---- "earlier{{ src_name_field }}" asc, "earlier{{ src_id_field }}" desc, "later{{ src_name_field }}" ASC, "later{{ src_id_field }}" desc
+
   -- "{{ fieldPrefix }}src_name", "{{ fieldPrefix }}src_id"
   --{% for order_by_field in order_by_fields %}
   --filtered_plus.{{ adapter.quote(order_by_field) }} {% if not loop.last %},{% endif %}
@@ -133,9 +167,12 @@ with link_candidates as (
 
 select * from all_merged
   order by
-  all_merged."{{ src_name_field }}" asc, all_merged."{{ src_id_field }}" desc
+    ---- all_merged."{{ src_name_field }}" asc, all_merged."{{ src_id_field }}" desc
+    {% for order_by_field in order_by_fields %}
+      all_merged."{{ order_by_field }}" desc {% if not loop.last%},{% endif %}
+    {% endfor %}
   --{% for order_by_field in order_by_fields %}
   --all_merged.{{ adapter.quote(order_by_field) }} {% if not loop.last %},{% endif %}
   --{% endfor %}
-          
+  
 {% endmacro %}
