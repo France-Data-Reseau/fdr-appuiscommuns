f9d5f1c8b8cac49c141fb30b8b90778c8210302b
Marc Dutoo <marc.dutoo@gmail.com>
2022-04-29 17:49:12 +0200

    dedup : genericized criteria

--- macros/apcom/apcom_supportaerien__dedupe_geometry.sql
+++ macros/apcom/apcom_supportaerien__dedupe_geometry.sql
@@ -7,7 +7,7 @@ TODO and additional order fields
 TODO example
 #}
 
-{% macro apcom_supportaerien_translation__dup_geometry(normalized_source_model_name, id_field, fields, order_by_fields, distance_m=20) %}
+{% macro apcom_supportaerien_translation__dup_geometry(normalized_source_model_name, id_field, src_name_field, src_id_field, fields, criteria, order_by_fields2) %}
 
 {% set containerUrl = 'http://' + 'datalake.francedatareseau.fr' %}
 {% set typeUrlPrefix = containerUrl + '/dc/type/' %}
@@ -23,50 +23,60 @@ TODO example
 {% set fieldPrefix = prefix + '__' %}
 {% set idUrlPrefix = typeUrlPrefix + type + '/' %}
 
+{% set id_fields = [id_field] %}
+{% set order_by_fields = [src_name_field, src_id_field] %}
+{% set id_and_order_by_fields = id_fields + order_by_fields %}
+
 with link_candidates as (
     -- xs on 1m lines
     select
-        earlier."{{ fieldPrefix }}src_name" as earlier_src_name,
-        earlier."{{ fieldPrefix }}src_id" as earlier_src_id, -- to be able to order before group by (TODO Q but does it use it ?)
-        earlier."{{ fieldPrefix }}Id" as earlier_id,
-        later."{{ fieldPrefix }}src_name" as later_src_name,
-        later."{{ fieldPrefix }}src_id" as later_src_id,
-        later."{{ fieldPrefix }}Id" as later_id
-        , earlier.geometry as earlier_geometry, later.geometry as later_geometry -- for debugging purpose
+        {# % for field in id_and_order_by_fields %}
+          earlier."{{ field }}" as "earlier{{ field }}", later."{{ field }}" as "later{{ field }}" {% if not loop.last%},{% endif %}
+        {% endfor % #}
+
+        earlier."{{ src_name_field }}" as "earlier{{ src_name_field }}",
+        earlier."{{ src_id_field }}" as "earlier{{ src_id_field }}", -- to be able to order before group by (TODO Q but does it use it ?)
+        earlier."{{ id_field }}" as "earlier{{ id_field }}",
+        later."{{ src_name_field }}" as "later{{ src_name_field }}",
+        later."{{ src_id_field }}" as "later{{ src_id_field }}",
+        later."{{ id_field }}" as "later{{ id_field }}"
+
+        --, earlier.geometry as earlier_geometry, later.geometry as later_geometry -- for debugging purpose
     FROM {{ ref(normalized_source_model_name) }} earlier, {{ ref(normalized_source_model_name) }} later
-    WHERE earlier."{{ fieldPrefix }}src_name" < later."{{ fieldPrefix }}src_name" -- only between different sources
-    --and earlier."{{ fieldPrefix }}src_id" < later."{{ fieldPrefix }}src_id" -- ONLY IF WERE IN SAME SOURCE assuming _src_id is the model's ordering field
-    and ST_Distance(ST_Transform(earlier.geometry, 3857), ST_Transform(later.geometry, 3857)) < {{ distance_m }} -- requires transform because 4326 distance is in degrees ; assuming geometry's not NULL
+    WHERE earlier."{{ src_name_field }}" < later."{{ src_name_field }}" -- only between different sources
+    --and earlier."{{ src_id_field }}" < later."{{ src_id_field }}" -- ONLY IF WERE IN SAME SOURCE assuming _src_id is the model's ordering field
+    and {{ criteria }}
+    --ST_Distance(ST_Transform(earlier.geometry, 3857), ST_Transform(later.geometry, 3857)) < {{ distance_m }} -- requires transform because 4326 distance is in degrees ; assuming geometry's not NULL
     -- within box is more efficient https://postgis.net/workshops/postgis-intro/knn.html :
     --and ST_Expand(earlier.geometry, {{ distance_m }}) && later.geometry -- TODO REQUIRES CONVERT and then ST_Within faster
     -- see https://gis.stackexchange.com/questions/93936/searching-planet-osm-point-by-longitude-and-latitude/93957#93957 https://gis.stackexchange.com/questions/94886/st-expand-return-different-results-depending-on-meters
     --and ST_DWithin(way, ST_Transform(later.geometry, 3857),
-    ORDER BY earlier."{{ fieldPrefix }}src_name" asc, earlier."{{ fieldPrefix }}src_id" desc,
-    later."{{ fieldPrefix }}src_name" asc, later."{{ fieldPrefix }}src_id" desc
+    ORDER BY earlier."{{ src_name_field }}" asc, earlier."{{ src_id_field }}" desc,
+    later."{{ src_name_field }}" asc, later."{{ src_id_field }}" desc
 
 ), filtered as (
 
   -- remove lines where later / right part has already been mentioned before as earlier / left part
   -- i.e. previous groups will already have included them :
   select * from link_candidates current_lc
-  where earlier_id not in (
-    select later_id from link_candidates lc_before_current_one
-    WHERE lc_before_current_one.earlier_src_name < current_lc.later_src_name -- only between different sources
-    --and earlier."{{ fieldPrefix }}src_id" < later."{{ fieldPrefix }}src_id" -- ONLY IF WERE IN SAME SOURCE assuming _src_id is the model's ordering field
+  where "earlier{{ id_field }}" not in (
+    select "later{{ id_field }}" from link_candidates lc_before_current_one
+    WHERE lc_before_current_one."earlier{{ src_name_field }}" < current_lc."later{{ src_name_field }}" -- only between different sources
+    --and earlier."{{ src_id_field }}" < later."{{ src_id_field }}" -- ONLY IF WERE IN SAME SOURCE assuming _src_id is the model's ordering field
   )
 
 ), filtered_plus as (
   select * from (
   select
-    earlier_src_name, earlier_src_id, earlier_id,
-    earlier_src_name as later_src_name, earlier_src_id as later_src_id,  earlier_id as later_id
-    , earlier_geometry as earlier_geometry, earlier_geometry as later_geometry -- for debugging purpose
+    "earlier{{ src_name_field }}", "earlier{{ src_id_field }}", "earlier{{ id_field }}",
+    "earlier{{ src_name_field }}" as "later{{ src_name_field }}", "earlier{{ src_id_field }}" as "later{{ src_id_field }}",  "earlier{{ id_field }}" as "later{{ id_field }}"
+    --, earlier_geometry as earlier_geometry, earlier_geometry as later_geometry -- for debugging purpose
     from filtered
   union
   select * from filtered
   ) fp
   -- order of preference of data among lines :
-  order by earlier_src_name asc, earlier_src_id desc, later_src_name, later_src_id
+  order by "earlier{{ src_name_field }}" asc, "earlier{{ src_id_field }}" desc, "later{{ src_name_field }}", "later{{ src_id_field }}"
   -- "{{ fieldPrefix }}src_name", "{{ fieldPrefix }}src_id"
   --{% for order_by_field in order_by_fields %}
   --filtered_plus.{{ adapter.quote(order_by_field) }} {% if not loop.last %},{% endif %}
@@ -75,11 +85,11 @@ with link_candidates as (
 ), to_be_merged_ids as (
 
   select
-    distinct earlier_id
+    distinct "earlier{{ id_field }}"
   from filtered
   union
   select
-    distinct later_id
+    distinct "later{{ id_field }}"
   from filtered
   -- (and once filtered no later_id is among earlier_id)
 
@@ -87,23 +97,23 @@ with link_candidates as (
 
   -- TODO and "updated" or row_count() for ORDER BY LIMIT 1 ? LATER macro & FILTER NOT NULL http///
   select
-      filtered_plus.earlier_id as "{{ id_field }}", -- !!! uuid does not support min() ; or (ARRAY_AGG("{{ id_field }}") FILTER (WHERE "{{ id_field }}" IS NOT NULL))[1] as "{{ id_field }}",
+      filtered_plus."earlier{{ id_field }}" as "{{ id_field }}", -- !!! uuid does not support min() ; or (ARRAY_AGG("{{ id_field }}") FILTER (WHERE "{{ id_field }}" IS NOT NULL))[1] as "{{ id_field }}",
       {% for field in fields | reject("eq", id_field) %}
         -- get not null appearing first according to src_name (ex. OSM last) and bigger src_id (most recent first) :
         -- order see https://stackoverflow.com/questions/7317475/postgresql-array-agg-order
-        (ARRAY_AGG(all1.{{ adapter.quote(field) }} ORDER BY earlier_src_name ASC, earlier_src_id DESC) FILTER (WHERE all1.{{ adapter.quote(field) }} IS NOT NULL))[1] as {{ adapter.quote(field) }},
+        (ARRAY_AGG(all1.{{ adapter.quote(field) }} ORDER BY earlier{{ src_name_field }} ASC, "earlier{{ src_id_field }}" DESC) FILTER (WHERE all1.{{ adapter.quote(field) }} IS NOT NULL))[1] as {{ adapter.quote(field) }},
       {% endfor %}
-      --filtered_plus.*,--"{{ fieldPrefix }}Id",
+      --filtered_plus.*,--"{{ id_field }}",
       -- debug :
       --(ARRAY_AGG("fdrcommune__insee_id") FILTER (WHERE "fdrcommune__insee_id" IS NOT NULL order by "updated" desc limit 1))[1] as "fdrcommune__insee_id",
       -- see : https://stackoverflow.com/questions/61874745/postgresql-get-first-non-null-value-per-group
       -- https://github.com/dbt-labs/dbt-utils/issues/335 https://github.com/dbt-labs/dbt-utils/pull/29
-      (ARRAY_AGG(filtered_plus.later_id ORDER BY earlier_src_name ASC, earlier_src_id DESC) FILTER (WHERE filtered_plus.later_id <> filtered_plus.earlier_id)) as "merged_ids",
-      (count(filtered_plus.later_id) - 1) as "merged_ids_nb"
+      (ARRAY_AGG(filtered_plus."later{{ id_field }}" ORDER BY "earlier{{ src_name_field }}" ASC, "earlier{{ src_id_field }}" DESC) FILTER (WHERE filtered_plus."later{{ id_field }}" <> filtered_plus."earlier{{ id_field }}")) as "merged_ids",
+      (count(filtered_plus."later{{ id_field }}") - 1) as "merged_ids_nb"
   from filtered_plus
-  join {{ ref(normalized_source_model_name) }} all1 on filtered_plus.later_id = all1."{{ id_field }}"
-  group by filtered_plus.earlier_id
-  -- NO ORDER BY ELSE error column "filtered_plus.earlier_src_name" must appear in the GROUP BY clause or be used in an aggregate function ; order of preference : by default of source then of its order field (unless all sources are dated)
+  join {{ ref(normalized_source_model_name) }} all1 on filtered_plus."later{{ id_field }}" = all1."{{ id_field }}"
+  group by filtered_plus."earlier{{ id_field }}"
+  -- NO ORDER BY ELSE error column "filtered_plus.earlier{{ src_name_field }}" must appear in the GROUP BY clause or be used in an aggregate function ; order of preference : by default of source then of its order field (unless all sources are dated)
 
 ), all_merged as (
 
@@ -123,7 +133,7 @@ with link_candidates as (
 
 select * from all_merged
   order by
-  all_merged."{{ fieldPrefix }}src_name" asc, all_merged."{{ fieldPrefix }}src_id" desc
+  all_merged."{{ src_name_field }}" asc, all_merged."{{ src_id_field }}" desc
   --{% for order_by_field in order_by_fields %}
   --all_merged.{{ adapter.quote(order_by_field) }} {% if not loop.last %},{% endif %}
   --{% endfor %}
--- models/apcom/apcom_supportaerien__deduped.sql
+++ models/apcom/apcom_supportaerien__deduped.sql
@@ -6,7 +6,7 @@ or osm_powsupp__apcom_supportaerien_translated
 
 {{
   config(
-    materialized="view"
+    materialized="table"
   )
 }}
 
@@ -14,4 +14,6 @@ or osm_powsupp__apcom_supportaerien_translated
 
 {%- set fields = ['_dbt_source_relation'] + adapter.get_columns_in_relation(ref('appuiscommuns_supportaerien__definition')) | map(attribute="name") | list -%}-- BEWARE without | list it stays a generator that can only be iterated once
 
-{{ apcom_supportaerien_translation__dup_geometry('appuiscommuns_supportaerien', fieldPrefix + "Id", fields, [fieldPrefix + "src_name", fieldPrefix + "src_id"], 20) }}
+{% set distance_m = 20 %}
+{% set criteria = "ST_Distance(ST_Transform(earlier.geometry, 3857), ST_Transform(later.geometry, 3857)) < " ~ distance_m ~ " -- requires transform because 4326 distance is in degrees ; assuming geometry is not NULL" %}
+{{ apcom_supportaerien_translation__dup_geometry('appuiscommuns_supportaerien', fieldPrefix + "Id", fieldPrefix + "src_name", fieldPrefix + "src_id", fields, criteria, [fieldPrefix + "src_name", fieldPrefix + "src_id"]) }}
