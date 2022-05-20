a0de56d47027aabc699012acad5c371da0b21dfc
Marc Dutoo <marc.dutoo@gmail.com>
2022-05-20 16:51:11 +0200

    source_or_test_ref : tries several example suffixes

--- macros/source_or_test_ref.sql
+++ macros/source_or_test_ref.sql
@@ -1,6 +1,7 @@
 {#
 inspired by https://servian.dev/unit-testing-in-dbt-part-1-d0cc20fd189a
 #}
+
 {% macro source_or_test_ref(opt_source_name, model_name) %}
 
       {% if target.name == 'test' %}
@@ -8,23 +9,32 @@ inspired by https://servian.dev/unit-testing-in-dbt-part-1-d0cc20fd189a
             {# if test, it's obligatorily a ref, that has been installed by a dbt run in this project,
             to a model provided by this project or one of its deps
             #}
-            {%- set test_ref = adapter.get_relation(
-                  database = this.database,
-                  schema = this.schema,
-                  identifier = model_name ~ '_extract') 
-            -%}
-            {{ return(test_ref) }}
-
-      {% else %}
-      
-            {% for relation in dbt_utils.get_relations_by_pattern(this.schema, model_name) %}
-                {# else either it's a ref, therefore created in this schema, even if defined by a dep package #}
-                {{ return(relation) }}
+            {% set test_ref = { 'test_ref' : none} %}
+            {% for suffix in var("test_extract_suffixes") %}
+              {% if test_ref.test_ref is none %}
+                {% if test_ref.update({'test_ref': adapter.get_relation(
+                      database = this.database,
+                      schema = this.schema,
+                      identifier = model_name ~ suffix) })
+                %}{% endif %}
+                -- macro source_or_test_ref {{ suffix }} get_relation('{{ model_name ~ suffix }}') : {{ test_ref.test_ref }}
+              {% else %}
+                {{ return(test_ref.test_ref) }}
+              {% endif %}
             {% endfor %}
-            
-            {# or a local source TODO and what if remote source ex. ODS communes ? #}
-            {{ return(source(opt_source_name, model_name)) }}
-      
+
+            {% if test_ref is not none %}
+              {{ return(test_ref.test_ref) }}
+            {% endif %}
+
       {% endif %}
+
+      {% for relation in dbt_utils.get_relations_by_pattern(this.schema, model_name) %}
+          {# else either it's a ref, therefore created in this schema, even if defined by a dep package #}
+          {{ return(relation) }}
+      {% endfor %}
+
+      {# or a local source TODO and what if remote source ex. ODS communes ? #}
+      {{ return(source(opt_source_name, model_name)) }}
  
 {% endmacro %}
