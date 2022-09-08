694520f4b3f6265e64e80e6ce0b1771aea478acb
Marc Dutoo <marc.dutoo@gmail.com>
2022-09-08 13:43:08 +0200

    import overview : added metrics over fdr_import_resource, to display in Superset

--- models/fdr_import/fdr_import_resource_view.sql
+++ models/fdr_import/fdr_import_resource_view.sql
@@ -12,4 +12,14 @@ https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom
   )
 }}
 
-select * from {{ source('fdr_import', 'fdr_import_resource') }}
+with import_resource as (
+  select * from {{ source('fdr_import', 'fdr_import_resource') }}
+
+), with_metrics as (
+    SELECT *,
+        (case when status = 'success' then 1 else 0 end ) As success,
+        (case when status = 'skipped' then 1 else 0 end ) As skipped,
+        (case when status = 'error' then 1 else 0 end ) As error
+    from import_resource
+)
+select * from with_metrics
